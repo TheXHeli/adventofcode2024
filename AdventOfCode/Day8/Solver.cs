9a401deea6412e2e9ab44c6d90f34379b284674d
Sebastian Thiede <st@comparit.de>
2024-12-08 22:26:04 +0100

    feat: solution for day 8.2

--- AdventOfCode/Day8/Solver.cs
+++ AdventOfCode/Day8/Solver.cs
@@ -48,8 +48,8 @@ public static class Solver
                         {
                             //if (mapLinesRaw[newY1][newX1] == '.')
                             //{
-                                resultMap[newX1, newY1] = true;
-                                Console.WriteLine($"Antinodes: {newX1}, {newY1}");
+                            resultMap[newX1, newY1] = true;
+                            Console.WriteLine($"Antinodes: {newX1}, {newY1}");
                             //}
                         }
                         //Console.WriteLine($"{s},{e}");
@@ -89,5 +89,67 @@ public static class Solver
 
     public static void SolveIt_2ndPart()
     {
+        var mapLinesRaw = File.ReadAllLines("inputs/day8_1.txt");
+        //byte[,] mapLinesByte = new byte[mapLinesRaw.Length, mapLinesRaw[0].Length];
+        var antennen = new Dictionary<char, List<(int, int)>>();
+        for (int y = 0; y < mapLinesRaw.Length; y++)
+        {
+            for (int x = 0; x < mapLinesRaw[y].Length; x++)
+            {
+                if (mapLinesRaw[y][x] == '.') continue;
+                if (!antennen.ContainsKey(mapLinesRaw[y][x]))
+                {
+                    antennen.Add(mapLinesRaw[y][x], new List<(int, int)>());
+                }
+
+                antennen[mapLinesRaw[y][x]].Add((x, y));
+            }
+        }
+
+        //---
+        var borderX = mapLinesRaw.Length;
+        var borderY = mapLinesRaw[0].Length;
+        bool[,] resultMap = new bool[borderX, borderY];
+        foreach (var antennenDaten in antennen)
+        {
+            var antennenListe = antennenDaten.Value;
+            for (int s = 0; s < antennenListe.Count; s++)
+            {
+                for (int e = 0; e < antennenListe.Count; e++)
+                {
+                    resultMap[antennenListe[s].Item1, antennenListe[s].Item2] = true; 
+                    resultMap[antennenListe[e].Item1, antennenListe[e].Item2] = true; 
+                    
+                    if (s != e)
+                    {
+                        var dX = antennenListe[s].Item1 - antennenListe[e].Item1;
+                        var dY = antennenListe[s].Item2 - antennenListe[e].Item2;
+                        var newStartX1 = antennenListe[s].Item1 + dX;
+                        var newStartY1 = antennenListe[s].Item2 + dY;
+                        var newX1 = newStartX1;
+                        var newY1 = newStartY1;
+                        //var newX2 = antennenListe[e].Item1 - dX;
+                        //var newY2 = antennenListe[e].Item2 - dY;
+                        while (newX1 >= 0 && newY1 >= 0 && newX1 < borderX && newY1 < borderY)
+                        {
+                            //wenn hier reingegangen wird, dann strahlt die Antenne (hat also mehr als eine) unbd kann dann als gesetzt betrachtet werden
+                            resultMap[antennenListe[s].Item1, antennenListe[s].Item2] = true; 
+                            resultMap[antennenListe[e].Item1, antennenListe[e].Item2] = true; 
+                            
+                            resultMap[newX1, newY1] = true;
+                            Console.WriteLine($"Antinodes: {newX1}, {newY1}");
+                            newX1 += dX;
+                            newY1 += dY;
+                        }
+
+
+                        //Console.WriteLine($"{s},{e}");
+                    }
+                }
+            }
+        }
+
+        var result = WriteResultMap(resultMap);
+        Console.WriteLine(result);
     }
 }
